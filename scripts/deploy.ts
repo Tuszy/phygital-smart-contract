c138733f6c65bb4a348b5febc64ae53f0c9b475a
Dennis T <denniro@gmail.com>
2023-11-02 19:14:18 +0100

    clean deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,26 +1,15 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const phygitalAsset = await ethers.deployContract("PhygitalAsset", [
+    /*constructor args*/
+  ]);
 
-  const lockedAmount = ethers.parseEther("0.001");
+  await phygitalAsset.waitForDeployment();
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(`Phygital asset deployed to ${phygitalAsset.target}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
