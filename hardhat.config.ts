02cbd3acbc29b976f317ca4bb1a5e6b72b06e74c
Dennis T <denniro@gmail.com>
2023-11-03 23:28:33 +0100

    fix hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,9 +3,8 @@ import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-contract-sizer";
 import "@openzeppelin/hardhat-upgrades";
 import "hardhat-deploy";
-import { LSPFactory } from "@lukso/lsp-factory.js";
+import { EthersExternalProvider, LSPFactory } from "@lukso/lsp-factory.js";
 import { ERC725 } from "@erc725/erc725.js";
-import { SignerOptions } from "@lukso/lsp-factory.js/build/main/src/lib/interfaces/lsp-factory-options";
 
 declare module "hardhat/types/runtime" {
   interface HardhatRuntimeEnvironment {
@@ -18,20 +17,17 @@ declare module "hardhat/types/runtime" {
 
 // Add Web3Provider to HRE
 extendEnvironment(async (hre) => {
-  hre.Web3Provider = new hre.ethers.BrowserProvider(hre.network.provider);
+  hre.Web3Provider = hre.network.provider;
+  hre.Web3Provider.chainId = hre.network.config.chainId;
 });
 
 // Add LSPFactory to HRE
 extendEnvironment(async (hre) => {
   hre.LSPFactory = LSPFactory;
-
-  const signerOptions: SignerOptions = {
-    deployKey: hre.network.config.accounts, // Private key of the account which will deploy smart contracts
-    chainId: hre.network.config.chainId,
-  };
-
-  // hre.network.provider is an EIP1193-compatible provider.
-  hre.lspFactory = new LSPFactory(hre.Web3Provider, signerOptions);
+  hre.lspFactory = new LSPFactory(
+    hre.Web3Provider as EthersExternalProvider,
+    hre.network.config.accounts[0].privateKey
+  );
 });
 
 // Add ERC725 to HRE
@@ -39,8 +35,57 @@ extendEnvironment(async (hre) => {
   hre.ERC725 = ERC725;
 });
 
+const DEFAULT_PRIVATE_KEY =
+  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10000,
+      },
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+  },
+  networks: {
+    lukso: {
+      url: "https://rpc.l16.lukso.network",
+      chainId: 2828,
+      accounts: [process.env.PRIVATE_KEY ?? DEFAULT_PRIVATE_KEY],
+    },
+    hardhat: {
+      chainId: 1337,
+      accounts: [
+        {
+          privateKey: process.env.PRIVATE_KEY ?? DEFAULT_PRIVATE_KEY,
+          balance: "10000000000000000000000", // 10000 ETH
+        },
+      ],
+    },
+    localhost: {
+      chainId: 1337,
+      accounts: [
+        DEFAULT_PRIVATE_KEY,
+        "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
+        "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
+        "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",
+        "0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a",
+      ],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      //lukso: "lukso api key for lukso-scan"
+    },
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
 };
 
 export default config;
