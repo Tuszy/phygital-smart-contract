90a126aa67410b37614b6fa83c1e757ef395b6b3
Dennis T <denniro@gmail.com>
2023-11-05 15:03:28 +0100

    adapt interface ids

--- contracts/PhygitalAssetConstants.sol
+++ contracts/PhygitalAssetConstants.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 // --- ERC165 interface ids
 bytes4 constant _INTERFACEID_PHYGITAL_ASSET_COLLECTION = 0x6d07b8f0;
-bytes4 constant _INTERFACEID_PHYGITAL_ASSET = 0x86b7b336;
+bytes4 constant _INTERFACEID_PHYGITAL_ASSET = 0xf2d3f8b7;
 
 // keccak256('PhygitalAssetCollectionMerkleTreeURI')
 bytes32 constant _PHYGITAL_ASSET_COLLECTION_MERKLE_TREE_URI_KEY = 0x931b4f026a670558cbd3aeba98023d97e4910a7f5be2441e3f9ec1c5c5a1001f;
